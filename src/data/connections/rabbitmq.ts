cdfcde56c195acbe43017a05952bd7835b323023
Lucas Santos <lhs.santoss@gmail.com>
2019-05-15 15:43:17 -0300

    Fix attempt number

--- src/data/connections/rabbitmq.ts
+++ src/data/connections/rabbitmq.ts
@@ -15,14 +15,14 @@ function sleep (timeout: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, timeout))
 }
 
-export async function createConnection ({ hostname, port, username, password, protocol, maximumConnectionAttempts, connectionRetryInterval }: IRabbitMQParams, attempsMade: number = 0): Promise<Connection> {
+export async function createConnection ({ hostname, port, username, password, protocol, maximumConnectionAttempts, connectionRetryInterval }: IRabbitMQParams, attempsMade: number = 1): Promise<Connection> {
   try {
-    if (attempsMade > 0) await sleep(attempsMade * connectionRetryInterval)
+    if (attempsMade > 1) await sleep(attempsMade * connectionRetryInterval)
     const connection = await amqp.connect({ protocol, hostname, password, port, username })
     console.log('MQTT Connected')
     return connection
   } catch (err) {
-    if (attempsMade >= maximumConnectionAttempts) throw new ConnectionError(err.message)
+    if (attempsMade >= maximumConnectionAttempts + 1) throw new ConnectionError(err.message)
     console.log(`Failed to connect to MQTT (${attempsMade}/${maximumConnectionAttempts}) "${err.message}" trying in ${(attempsMade * connectionRetryInterval) / 1000}s`)
     return createConnection({ hostname, port, username, password, protocol, maximumConnectionAttempts, connectionRetryInterval }, attempsMade + 1)
   }
